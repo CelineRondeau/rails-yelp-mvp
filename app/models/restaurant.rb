e68959c3fd795ff7d4049ce6c4e50f5511b590ff
CelineRondeau <rondeau9314@gmail.com>
2020-05-13 14:33:25 -0400

    Fixed error in models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  # name, an address and a category.
+  validates :name, presence :true
+  validates :address, presence :true
+  validates :category, presence :true
 end
