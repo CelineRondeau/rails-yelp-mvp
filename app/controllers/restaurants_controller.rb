a1fd4279611db6ea56859b9ffd52269966a275ce
CelineRondeau <rondeau9314@gmail.com>
2020-05-13 16:14:00 -0400

    Working on validation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
 end
