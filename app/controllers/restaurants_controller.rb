73a25d490f3dd81fd5604548fa3e33dcaaad4b31
CelineRondeau <rondeau9314@gmail.com>
2020-05-13 16:25:04 -0400

    Working on the controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,21 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.after_save :
+    
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,21 @@
 class ReviewsController < ApplicationController
+  #  GET /restaurants/:restaurant_id/reviews/new
   def new
+    @restaurant = restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
+  # POST /restaurants/:restaurant_id/reviews
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # render the new page with all the same data
+      render :new
+    end
   end
 
   private
