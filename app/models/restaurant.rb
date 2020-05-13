5c24e62471f377bc834da42ae23505c561adec1e
CelineRondeau <rondeau9314@gmail.com>
2020-05-13 14:52:40 -0400

    Finished the routes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   # name, an address and a category.
-  validates :name, presence :true
-  validates :address, presence :true
-  validates :category, presence :true
+  validates :name, presence (:true)
+  validates :address, presence (:true)
+  validates :category, presence (:true)
 end
